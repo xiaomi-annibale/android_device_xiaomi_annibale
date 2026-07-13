e7ca8926c6a97086ee0f8b2a914c202cf911878a
ramshell688 <ramonzost68@gmail.com>
2026-07-13 09:46:10 +0700

    Revert "annibale: Drop screen off UDFPS unlock"
    This reverts commit 79eece634cff98c96d0153e1452bc4a432077ddb.
    
    Change-Id: I20a24512225f04487d13be4213d5dde04dac587a

--- device.mk
+++ device.mk
@@ -371,7 +371,8 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors-service.xiaomi-multihal \
-    sensors.dynamic_sensor_hal
+    sensors.dynamic_sensor_hal \
+    sensors.xiaomi.v2
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
